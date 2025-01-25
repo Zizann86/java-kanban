7c78a0db15962d88b2b56df23d107c73ed6f2841
Alexey Zinchenko <zizann86@yandex.ru>
2025-01-25 11:49:57 +0500

    Итерация 8 Спринт6. исправление замечаний Ревьюера: убрал не нужный класс, исправил модификаторы доступа метода и расположение их.

--- src/InMemoryHistoryManager.java
+++ src/InMemoryHistoryManager.java
@@ -4,8 +4,70 @@ import java.util.List;
 import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final HandMadeLinkedList historyTasksForList = new HandMadeLinkedList();
+    private Node head;
+    private Node tail;
     private final Map<Integer, Node> historyTasksForMap = new HashMap<>();
+
+    @Override
+    public void addToHistory(Task task) {
+        if (historyTasksForMap.containsKey(task.getId())) {
+            removeNode(historyTasksForMap.get(task.getId()));
+        }
+        Node node = new Node(null, task, null);
+        linkLast(node);
+        historyTasksForMap.put(task.getId(), node);
+    }
+
+    @Override
+    public void remove(int id) {
+        if (historyTasksForMap.containsKey(id)) {
+            removeNode(historyTasksForMap.remove(id));
+        }
+    }
+
+    @Override
+    public List<Task> getHistory() {
+        return getTasks();
+    }
+
+   private void linkLast(Node node) {
+        final Node oldTail = tail;
+        node.prev = oldTail;
+        tail = node;
+        if (oldTail == null) {
+            head = node;
+        } else {
+            oldTail.next = node;
+        }
+    }
+
+   private void removeNode(Node node) {
+        Node prevNode = node.prev;
+        Node nextNode = node.next;
+        if (prevNode == null && nextNode == null) {
+            head = null;
+            tail = null;
+        } else if (prevNode == null) {
+            head = nextNode;
+            nextNode.prev = null;
+        } else if (nextNode == null) {
+            tail = prevNode;
+            prevNode.next = null;
+        } else {
+            prevNode.next = nextNode;
+            nextNode.prev = prevNode;
+        }
+    }
+
+   private List<Task> getTasks() {
+        List<Task> tasks = new ArrayList<>();
+        Node node = head;
+        while (node != null) {
+            tasks.add(node.data);
+            node = node.next;
+        }
+        return tasks;
+    }
 
     //Создаем вложенный класс Node:
     private static class Node {
@@ -13,78 +75,10 @@ public class InMemoryHistoryManager implements HistoryManager {
         public Node next;
         public Node prev;
 
-        public Node(Node prev, Task data, Node next) {
+        private Node(Node prev, Task data, Node next) {
             this.data = data;
             this.next = next;
             this.prev = prev;
         }
     }
-
-    @Override
-    public void addToHistory(Task task) {
-        if (historyTasksForMap.containsKey(task.getId())) {
-            historyTasksForList.removeNode(historyTasksForMap.get(task.getId()));
-        }
-        Node node = new Node(null, task, null);
-        historyTasksForList.linkLast(node);
-        historyTasksForMap.put(task.getId(), node);
-    }
-
-    @Override
-    public void remove(int id) {
-        if (historyTasksForMap.containsKey(id)) {
-            historyTasksForList.removeNode(historyTasksForMap.remove(id));
-        }
-    }
-
-    @Override
-    public List<Task> getHistory() {
-        return historyTasksForList.getTasks();
-    }
-
-    //Создаем свою реализацию LinkedList:
-    private static class HandMadeLinkedList {
-        private Node head;
-        private Node tail;
-
-        void linkLast(Node node) {
-            final Node oldTail = tail;
-            node.prev = oldTail;
-            tail = node;
-            if (oldTail == null) {
-                head = node;
-            } else {
-                oldTail.next = node;
-            }
-        }
-
-        void removeNode(Node node) {
-            Node prevNode = node.prev;
-            Node nextNode = node.next;
-            if (prevNode == null && nextNode == null) {
-                head = null;
-                tail = null;
-            } else if (prevNode == null) {
-                head = nextNode;
-                nextNode.prev = null;
-            } else if (nextNode == null) {
-                tail = prevNode;
-                prevNode.next = null;
-            } else {
-                prevNode.next = nextNode;
-                nextNode.prev = prevNode;
-            }
-        }
-
-        List<Task> getTasks() {
-            List<Task> tasks = new ArrayList<>();
-            Node node = head;
-            while (node != null) {
-                tasks.add(node.data);
-                node = node.next;
-            }
-            return tasks;
-        }
-    }
-
-    }
+}
